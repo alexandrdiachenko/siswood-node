f6052ec08693e42af3ad34d38665e83214f4f566
Alexandr Dyachenko <alexandrdiachenko@hotmail.com>
2020-05-07 15:42:33 +0300

    fix json RF banks

--- server/api/banks/bankState.js
+++ server/api/banks/bankState.js
@@ -80,59 +80,115 @@ export default {
       ]
     }
   ],
-  ru: {
-    sberbank: {
-      usdSell: 0,
-      usdBuy: 0,
-      eurSell: 0,
-      eurBuy: 0,
-      gbpSell: 0,
-      gbpBuy: 0,
-      chfSell: 0,
-      chfBuy: 0,
-      jpySell: 0,
-      jpyBuy: 0,
-      cnySell: 0,
-      cnyBuy: 0
+  ru: [
+    {
+      name: 'Sberbank',
+      currancy: [
+        {
+          name: 'USD',
+          USD: [0, 0]
+        },
+        {
+          name: 'EUR',
+          EUR: [0, 0]
+        },
+        {
+          name: 'GPB',
+          GPB: [0, 0]
+        },
+        {
+          name: 'CHF',
+          CHF: [0, 0]
+        },
+        {
+          name: 'JPY',
+          JPY: [0, 0]
+        },
+        {
+          name: 'CNY',
+          CNY: [0, 0]
+        }
+      ]
     },
-    centralbank: {
-      usdOnly: 0,
-      eurOnly: 0,
-      gbpOnly: 0,
-      chfOnly: 0,
-      jpyOnly: 0,
-      cnyOnly: 0,
-      cadOnly: 0,
-      sekOnly: 0,
-      plnOnly: 0
+    {
+      name: 'CentralBank',
+      currancy: [
+        {
+          name: 'USD',
+          USD: [0]
+        },
+        {
+          name: 'EUR',
+          EUR: [0]
+        },
+        {
+          name: 'GPB',
+          GPB: [0]
+        },
+        {
+          name: 'CHF',
+          CHF: [0, 0]
+        },
+        {
+          name: 'JPY',
+          JPY: [0, 0]
+        },
+        {
+          name: 'CNY',
+          CNY: [0, 0]
+        },
+        {
+          name: 'CAD',
+          CAD: [0, 0]
+        },
+        {
+          name: 'SEK',
+          SEK: [0, 0]
+        }
+      ],
+      pln: [
+        {
+          name: 'Narodowy Bank Polski',
+          currancy: [
+            {
+              name: 'USD',
+              USD: [0, 0]
+            },
+            {
+              name: 'AUD',
+              AUD: [0, 0]
+            },
+            {
+              name: 'CAD',
+              CAD: [0, 0]
+            },
+            {
+              name: 'EUR',
+              EUR: [0, 0]
+            },
+            {
+              name: 'HUF',
+              HUF: [0, 0]
+            },
+            {
+              name: 'CHF',
+              CHF: [0, 0]
+            },
+            {
+              name: 'DKK',
+              DKK: [0, 0]
+            },
+            {
+              name: 'NOK',
+              NOK: [0, 0]
+            },
+            {
+              name: 'SEK',
+              SEK: [0, 0]
+            }
+          ]
+        }
+      ]
     }
-  },
-  pl: {
-    narodowybankpolski: {
-      usdSell: 0,
-      usdBuy: 0,
-      audSell: 0,
-      audBuy: 0,
-      cadSell: 0,
-      cadBuy: 0,
-      eurSell: 0,
-      eurBuy: 0,
-      hufSell: 0,
-      hufBuy: 0,
-      chfSell: 0,
-      chfBuy: 0,
-      gbpSell: 0,
-      gbpBuy: 0,
-      jpySell: 0,
-      jpyBuy: 0,
-      czkSell: 0,
-      czkBuy: 0,
-      dkkSell: 0,
-      dkkBuy: 0,
-      nokSell: 0,
-      nokBuy: 0,
-      sekSell: 0,
-      sekBuy: 0
-    }
-  }
+  ]
 }
--- server/api/banks/update/location/russia.js
+++ server/api/banks/update/location/russia.js
@@ -6,19 +6,30 @@ const getSberbankRu = () => {
     'https://www.sberbank.ru/portalserver/proxy/?pipe=shortCachePipe&url=http://localhost/rates-web/rateService/rate/current%3FregionId%3D55%26currencyCode=826%26currencyCode=756%26currencyCode=392%26currencyCode=156%26currencyCode=840%26currencyCode=978%26rateCategory%3Dbeznal'
   )
     .then((response) => {
-      bankState.ru.sberbank.usdBuy = round(response.beznal['840'][0].buyValue)
-      bankState.ru.sberbank.usdSell = round(response.beznal['840'][0].sellValue)
-      bankState.ru.sberbank.eurBuy = round(response.beznal['978'][0].buyValue)
-      bankState.ru.sberbank.eurSell = round(response.beznal['978'][0].sellValue)
-      bankState.ru.sberbank.gbpBuy = round(response.beznal['826'][0].buyValue)
-      bankState.ru.sberbank.gbpSell = round(response.beznal['826'][0].sellValue)
-      bankState.ru.sberbank.chfBuy = round(response.beznal['756'][0].buyValue)
-      bankState.ru.sberbank.chfSell = round(response.beznal['756'][0].sellValue)
-      bankState.ru.sberbank.jpyBuy = round(response.beznal['392'][0].buyValue)
-      bankState.ru.sberbank.jpySell = round(response.beznal['392'][0].sellValue)
-      bankState.ru.sberbank.cnyBuy = round(response.beznal['156'][0].buyValue)
-      bankState.ru.sberbank.cnySell = round(response.beznal['156'][0].sellValue)
-      console.log(bankState.ru.sberbank)
+      bankState.ru[0].currancy[0].USD = [
+        round(response.beznal['840'][0].buyValue),
+        round(response.beznal['840'][0].sellValue)
+      ]
+      bankState.ru[0].currancy[1].EUR = [
+        round(response.beznal['978'][0].buyValue),
+        round(response.beznal['978'][0].sellValue)
+      ]
+      bankState.ru[0].currancy[2].GBP = [
+        round(response.beznal['826'][0].buyValue),
+        round(response.beznal['826'][0].sellValue)
+      ]
+      bankState.ru[0].currancy[3].CHF = [
+        round(response.beznal['756'][0].buyValue),
+        round(response.beznal['756'][0].sellValue)
+      ]
+      bankState.ru[0].currancy[4].JPY = [
+        round(response.beznal['392'][0].buyValue),
+        round(response.beznal['392'][0].sellValue)
+      ]
+      bankState.ru[0].currancy[5].CNY = [
+        round(response.beznal['156'][0].buyValue),
+        round(response.beznal['156'][0].sellValue)
+      ]
     })
     .catch((err) => {
       console.log('sberbank err : ', err.response.statusText, err.response.status)
@@ -28,16 +39,15 @@ const getSberbankRu = () => {
 const centralbankRu = () => {
   return getResponse('https://www.cbr-xml-daily.ru/daily_json.js')
     .then((response) => {
-      bankState.ru.centralbank.usdOnly = round(response.Valute.USD.Value)
-      bankState.ru.centralbank.eurOnly = round(response.Valute.EUR.Value)
-      bankState.ru.centralbank.gbpOnly = round(response.Valute.GBP.Value)
-      bankState.ru.centralbank.chfOnly = round(response.Valute.CHF.Value)
-      bankState.ru.centralbank.jpyOnly = round(response.Valute.JPY.Value)
-      bankState.ru.centralbank.cnyOnly = round(response.Valute.CNY.Value)
-      bankState.ru.centralbank.cadOnly = round(response.Valute.CAD.Value)
-      bankState.ru.centralbank.sekOnly = round(response.Valute.SEK.Value)
-      bankState.ru.centralbank.plnOnly = round(response.Valute.PLN.Value)
-      console.log(bankState.ru.centralbank)
+      bankState.ru[1].currancy[0].USD = [round(response.Valute.USD.Value)]
+      bankState.ru[1].currancy[1].EUR = [round(response.Valute.EUR.Value)]
+      bankState.ru[1].currancy[2].GBP = [round(response.Valute.GBP.Value)]
+      bankState.ru[1].currancy[3].CHF = [round(response.Valute.CHF.Value)]
+      bankState.ru[1].currancy[4].JPY = [round(response.Valute.JPY.Value)]
+      bankState.ru[1].currancy[5].CNY = [round(response.Valute.CNY.Value)]
+      bankState.ru[1].currancy[6].CAD = [round(response.Valute.CAD.Value)]
+      bankState.ru[1].currancy[7].SEK = [round(response.Valute.SEK.Value)]
+      bankState.ru[1].currancy[8].PLN = [round(response.Valute.PLN.Value)]
     })
     .catch((err) => {
       console.log('centralbank err : ', err.response.statusText, err.response.status)
