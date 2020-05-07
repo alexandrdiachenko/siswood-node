55248d59de7e249e679fdb66193690666da14e2c
Alexandr Dyachenko <alexandrdiachenko@hotmail.com>
2020-05-07 15:51:16 +0300

    fix json Poland  banks

--- server/api/banks/update/location/poland.js
+++ server/api/banks/update/location/poland.js
@@ -4,25 +4,42 @@ import bankState from '../../bankState'
 const getNarodowybankpolskiPl = () => {
   return getResponse('https://api.nbp.pl/api/exchangerates/tables/c/?format=json')
     .then((response) => {
-      bankState.pl.narodowybankpolski.usdBuy = round(response[0].rates[0].bid)
-      bankState.pl.narodowybankpolski.usdSell = round(response[0].rates[0].ask)
-      bankState.pl.narodowybankpolski.audBuy = round(response[0].rates[1].bid)
-      bankState.pl.narodowybankpolski.audSell = round(response[0].rates[1].ask)
-      bankState.pl.narodowybankpolski.cadBuy = round(response[0].rates[2].bid)
-      bankState.pl.narodowybankpolski.cadSell = round(response[0].rates[2].ask)
-      bankState.pl.narodowybankpolski.eurBuy = round(response[0].rates[3].bid)
-      bankState.pl.narodowybankpolski.eurSell = round(response[0].rates[3].ask)
-      bankState.pl.narodowybankpolski.hufBuy = round(response[0].rates[4].bid)
-      bankState.pl.narodowybankpolski.hufSell = round(response[0].rates[4].ask)
-      bankState.pl.narodowybankpolski.chfBuy = round(response[0].rates[5].bid)
-      bankState.pl.narodowybankpolski.chfSell = round(response[0].rates[5].ask)
-      bankState.pl.narodowybankpolski.dkkBuy = round(response[0].rates[6].bid)
-      bankState.pl.narodowybankpolski.dkkSell = round(response[0].rates[6].ask)
-      bankState.pl.narodowybankpolski.nokBuy = round(response[0].rates[7].bid)
-      bankState.pl.narodowybankpolski.nokSell = round(response[0].rates[7].ask)
-      bankState.pl.narodowybankpolski.sekBuy = round(response[0].rates[8].bid)
-      bankState.pl.narodowybankpolski.sekSell = round(response[0].rates[8].ask)
-      console.log(bankState.pl.narodowybankpolski)
+      bankState.pl[0].currancy[0].USD = [
+        round(response[0].rates[0].bid),
+        round(response[0].rates[0].ask)
+      ]
+      bankState.pl[0].currancy[1].AUD = [
+        round(response[0].rates[1].bid),
+        round(response[0].rates[1].ask)
+      ]
+      bankState.pl[0].currancy[2].CAD = [
+        round(response[0].rates[2].bid),
+        round(response[0].rates[2].ask)
+      ]
+      bankState.pl[0].currancy[3].EUR = [
+        round(response[0].rates[3].bid),
+        round(response[0].rates[3].ask)
+      ]
+      bankState.pl[0].currancy[4].HUF = [
+        round(response[0].rates[4].bid),
+        round(response[0].rates[4].ask)
+      ]
+      bankState.pl[0].currancy[5].CHF = [
+        round(response[0].rates[5].bid),
+        round(response[0].rates[5].ask)
+      ]
+      bankState.pl[0].currancy[6].DKK = [
+        round(response[0].rates[6].bid),
+        round(response[0].rates[6].ask)
+      ]
+      bankState.pl[0].currancy[7].NOK = [
+        round(response[0].rates[7].bid),
+        round(response[0].rates[7].ask)
+      ]
+      bankState.pl[0].currancy[8].SEK = [
+        round(response[0].rates[8].bid),
+        round(response[0].rates[8].ask)
+      ]
     })
     .catch((err) => {
       console.log('sberbank err : ', err.response.statusText, err.response.status)
